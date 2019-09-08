7035dafe6df62a8c5176ab05cf665aff2ed82b5a
Anthony Posselli <anthony.posselli@gmail.com>
2019-09-07 20:27:28 -0600

    Add asteroids.
    Add random asteroid spawning. Add asteroid movement. Add a demolish method to the asteroids to break them into smaller asteroids. Begin a new wave with more asteroids when no more asteroids are remaining. As a temporary means of showing the asteroids breaking and waves, the space bar destroys asteroids when pressed.

---
+++ asteroid.js
@@ -0,0 +1,95 @@
+const sizes = {
+    LARGE: 2,
+    MEDIUM: 1,
+    SMALL: 0
+}
+
+var Asteroid = new Phaser.Class({
+    Extends: Phaser.GameObjects.Image,
+
+    initialize:
+
+    function Asteroid(scene, posX, posY, size) {
+        Phaser.GameObjects.Image.call(this, scene)
+
+        this.setTexture('asteroid');
+
+        // Change the scale and speed of the asteroids to match the size of the asteroid.
+        // Smaller asteroids vary more in speed and can move faster.
+        switch (size) {
+            case sizes.LARGE:
+                this.setScale(0.2);
+                this.speed = 0.05;
+                break;
+            case sizes.MEDIUM:
+                this.setScale(0.1);
+                this.speed = 0.05 + 0.05 * Math.random();
+                break;
+            case sizes.SMALL:
+                this.setScale(0.05);
+                this.speed = 0.05 + 0.15 * Math.random();
+                break;
+        }
+
+        // Randomize the rotation of the asteroid
+        this.setAngle(Math.random() * 360);
+
+        // Randomly place the asteroid at the border of the screen
+        // TODO: Replace hard-coded screen bounds
+        if (posX == -1 && posY == -1) {
+            var perimeter = 2 * (800 - this.displayWidth) + 2 * (600 - this.displayHeight);
+            var placement = perimeter * Math.random();
+            if (placement < 2 * (800 - this.displayWidth)) {
+                if (placement < (800 - this.displayWidth)) {
+                    posX = placement + this.displayWidth / 2;
+                    posY = this.displayHeight / 2;
+                } else {
+                    posX = placement - (800 - this.displayWidth / 2);
+                    posY = (600 - this.displayHeight / 2);
+                }
+            } else {
+                placement -= 2 * (800 - this.displayWidth);
+                if (placement < (600 - this.displayHeight)) {
+                    posX = this.displayWidth / 2;
+                    posY = placement + this.displayHeight / 2;
+                } else {
+                    posX = (800 - this.displayWidth / 2);
+                    posY = placement - (600 - this.displayHeight / 2);
+                }
+            }
+        }
+
+        this.setPosition(posX, posY);
+        this.setOrigin(0.5);
+
+        this.size = size;
+
+        // Randomize the direction of the asteroid
+        var dirX = Math.random() - 0.5;
+        var dirY = Math.random() - 0.5;
+        this.direction = new Phaser.Math.Vector2(dirX, dirY);
+        this.direction.normalize();
+    },
+
+    move: function (delta) {
+        // Move the asteroid based on the time passed. Wrap around when reaching the screen edge.
+        var x = Phaser.Math.Wrap(this.x + delta * this.speed * this.direction.x,
+            this.displayWidth / 2, 800 - this.displayWidth / 2);
+        var y = Phaser.Math.Wrap(this.y + delta * this.speed * this.direction.y,
+            this.displayHeight / 2, 600 - this.displayHeight / 2);
+        this.setPosition(x, y);
+    },
+
+    demolish: function (scene, asteroids) {
+        // If the asteroid isn't the smallest size, create 2 new asteroids in its location
+        if (this.size > sizes.SMALL) {
+            for (i = 0; i < 2; i++) {
+                asteroids.add(new Asteroid(scene, this.x, this.y, this.size - 1), true);
+            }
+        }
+
+        // Kill the current asteroid
+        // TODO: Play death animation before hiding
+        asteroids.killAndHide(this);
+    }
+});
--- game.js
+++ game.js
@@ -18,14 +18,49 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('bg', 'assets/background.png')
+    this.load.image('bg', 'assets/background.png');
+    this.load.image('asteroid', 'assets/Asteroid.png');
 }
 
 function create() {
     this.image = this.add.image(800, 600, 'bg');
-    this.title = this.add.text(400, 300, 'Asteroids', {fontFamily: "Times New Roman"});
+    this.title = this.add.text(400, 300, 'Asteroids', { fontFamily: "Times New Roman" });
+
+    asteroidSpawnNum = 4; // The number of asteroids to spawn next wave, initially 4
+    asteroids = this.add.group(); // The group of asteroids
+    cursors = this.input.keyboard.createCursorKeys(); // The state of the keyboard
+
+
+    index = 0; // TODO: Remove later. Temporarily used to show asteroid demolition.
 }
 
-function update() {
+function update(time, delta) {
+    // Move all the asteroids
+    asteroids.getChildren().forEach(function (asteroid) {
+        asteroid.move(delta);
+    });
 
+    // Begin the next wave if all asteroids are gone
+    if (asteroids.countActive(true) == 0) {
+        // Clear out inactive asteroids, remove them from the scene and destroy them
+        asteroids.clear(true, true);
+        index = 0;
+
+        // Spawn new asteroids
+        for (i = 0; i < asteroidSpawnNum; i++) {
+            asteroids.add(new Asteroid(this, -1, -1, sizes.LARGE), true);
+        }
+
+        // Make next wave spawn 2 more asteroids than this one (this is what it looked like Asteroids was doing)
+        asteroidSpawnNum += 2;
+    }
+
+
+    // TODO: Replace once we have the ship and collisions/bullets in.
+    // This is a temporary demonstration of the demolition of asteroids.
+    // Press the space bar to destroy asteroids.
+    if (cursors.space.isDown) {
+        asteroids.getChildren()[index].demolish(this, asteroids);
+        index++;
+    }
 }
