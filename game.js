057739e3b3c30f957ee41a8c5a639069b00d1ee5
Trevor Alex <pascerveau@gmail.com>
2019-09-10 00:26:47 -0600

    add background music

--- game.js
+++ game.js
@@ -20,13 +20,18 @@ var game = new Phaser.Game(config);
 function preload() {
     this.load.image('bg', 'assets/background.png');
     this.load.image('asteroid', 'assets/Asteroid.png');
-    this.load.audio('shootAudio', 'assets/shoot.wav');
+    this.load.audio('shootAudio', 'assets/MissileFire.wav');
+    this.load.audio('explodeAudio', 'assets/AsteroidExplode.wav');
+    this.load.audio('bgm', 'assets/Blue_Sizzle_Madness_Paranoia.mp3');
 }
 
 function create() {
     this.image = this.add.image(800, 600, 'bg');
     this.title = this.add.text(400, 300, 'Asteroids', { fontFamily: "Times New Roman" });
     shootAudio = this.sound.add('shootAudio');
+    explodeAudio = this.sound.add('explodeAudio');
+    backgroundMusic = this.sound.add('bgm');
+    backgroundMusic.play({loop: true, volume: 0.3});
 
     asteroidSpawnNum = 4; // The number of asteroids to spawn next wave, initially 4
     asteroids = this.add.group(); // The group of asteroids
